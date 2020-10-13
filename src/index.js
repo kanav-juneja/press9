a3fb0ac18d50bf6ccebbd22911dd960d4f841c1f
Kanav Juneja <kanavjuneja@Kanavs-MacBook-Pro.local>
2020-10-13 10:43:45 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { createGlobalStyle } from 'styled-components'
+import { Normalize } from 'styled-normalize'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    body {
+      font-family: 'Open Sans';
+      font-size: 300;
+      font-size: 16px;
+    }
+  }
+`
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	<React.StrictMode>
+		<Normalize />
+		<GlobalStyle />
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
